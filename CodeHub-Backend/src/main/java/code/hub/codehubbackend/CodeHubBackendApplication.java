2e20af6c6bafef9dd5dfda3dccfdac0572864bd4
giasinguyen <giasinguyentran@gmail.com>
2025-06-08 21:53:34 +0700

    [CodeHub] First Commit !

---
+++ CodeHub-Backend/src/main/java/code/hub/codehubbackend/CodeHubBackendApplication.java
@@ -0,0 +1,13 @@
+package code.hub.codehubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeHubBackendApplication.class, args);
+    }
+
+}
---
+++ CodeHub-Backend/src/test/java/code/hub/codehubbackend/CodeHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package code.hub.codehubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

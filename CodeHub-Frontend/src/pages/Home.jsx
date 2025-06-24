6f52b066900159c948f5926c1198acbfd5d0c72f
giasinguyen <giasinguyentran@gmail.com>
2025-06-24 23:07:29 +0700

    fix: Update link for creating snippets to a more concise path

--- CodeHub-Frontend/src/pages/Home.jsx
+++ CodeHub-Frontend/src/pages/Home.jsx
@@ -274,7 +274,7 @@ const Home = () => {  const { refreshTrigger } = useSnippet();
               Build something amazing together.
             </p>
               <div className="flex flex-col sm:flex-row gap-4 justify-center mb-12">
-              <Link to="/create-snippet">
+              <Link to="/create">
                 <Button size="lg" className="px-8 py-4">
                   <Zap className="w-5 h-5 mr-2" />
                   Start Coding
